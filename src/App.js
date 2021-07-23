f350fd7c67ec46cffd30453cfdc6b94994a52793
rwishavg <grwishav34@gmail.com>
2021-07-23 22:42:25 +0530

    winner message display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Board from "./components/Board";
+import {calculateWinner} from "./helper";
 import "./styles/root.scss"
 
 const App = () => {
@@ -7,8 +8,13 @@ const App = () => {
   const [board,setBoard] = useState(Array(9).fill(null))
   const [isXnext,setIsXnext] = useState(false);
 
+  const winner = calculateWinner(board);
+
+  const message = (winner) ? `${winner} won!` : `The Next Player is ${isXnext? 'X' : 'O'}`;
+  console.log(message);
+  
   const handleClickSquare = (position) =>{
-    if(board[position]!=null){
+    if(board[position]!=null || winner != null){
       return
     }
     setBoard( (prev) => {
@@ -25,7 +31,7 @@ const App = () => {
   return (
     <div className="app">
     <h1>Tic Tac Toe !</h1>
-    <h2>message</h2>
+    <h2>{message}</h2>
     <Board board={board} handleClickSquare={handleClickSquare}/>
     </div>
   );
